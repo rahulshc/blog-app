41170dcfdc8126c8a719b71b750880895b6b6bd4
Swastika Library <rahul1.shc@gmail.com>
2020-01-16 16:50:04 +0530

    implemented lazy routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../Posts/Posts';
-import NewPost from '../NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+//import NewPost from '../NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(()=>{
+    return import('../NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
@@ -39,7 +44,7 @@ class Blog extends Component {
                 
                 <Switch>
                 
-                {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
+                {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null }
                 <Route path="/posts" component={Posts} />
                 <Route render ={() => <h1>Not Found</h1>} />
                 {/*handling 404 routes above this should always come last*/}
